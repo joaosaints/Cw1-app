2e0d2048b346b86cb4bf19883a020fb782ae5302
Joao Santos <jh1654@live.mdx.ac.uk>
2023-11-09 19:33:23 +0000

    fontawesome methods computed added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let schoolapp = new Vue({
     el: "#app",
-    data: {},
-    methods: {},
+    data: {
+        sitename: "After School Club app",
+        lesson: {
+            id: 1,
+            title: "Maths",
+            location: "London",
+            price: 50,
+            space: 5,
+            image: ""
+        },
+        cart: [],
+    },
+    methods: {
+        addItemToCart: function () {
+            this.cart.push(this.lesson.id);
+        }
+    },
+    computed: {
+        itemInTheCart: function () {
+            return this.cart.lenght || "";
+        }
+    }
 });
